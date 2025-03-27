89acd35842a0db3ce46aa7d8d53e92fc83f1dd41
BungusUngus <xavier.katzakis@hotmail.com>
2025-03-27 16:12:42 +1100

    UI
    Started  working on UI functionality

---
+++ Assets/Scripts/UI/UILogic.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace UI
+{
+    public class UILogic : MonoBehaviour
+    {
+        public GameObject panelToControl;
+        public bool playing;
+        public bool paused;
+        public GameObject panelTrigger;
+
+        //public bool isActive;
+
+        float _timer;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+            _timer = 0;
+            playing = true;
+            
+            DisablePanel();
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (playing)
+            {
+                _timer += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    paused = !paused;
+                }
+                
+            }
+
+            while (paused)
+            {
+                playing = false;
+            }
+
+
+            Time.timeScale = paused ? 0 : 1;
+        }
+
+        private void DisablePanel()
+        {
+            
+             panelToControl.SetActive(false);
+        }
+
+        private void EnablePanel()
+        {
+            panelToControl.SetActive(true);
+        }
+
+        public void OnTriggerEnter(Collider trigger)
+        {
+            if (trigger.transform.CompareTag("UITrigger"))
+            {
+                EnablePanel();
+                playing = false;
+            }
+        }
+    }
+    
+}
